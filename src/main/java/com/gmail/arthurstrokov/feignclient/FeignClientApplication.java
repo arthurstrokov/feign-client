d760090ed2ea2206867ea5c9490ea51fb1fd0793
Arthur <arthurstrokov@gmail.com>
2022-10-26 23:54:01 +0300

    up clean FeignClientApplication.java

--- src/main/java/com/gmail/arthurstrokov/feignclient/FeignClientApplication.java
+++ src/main/java/com/gmail/arthurstrokov/feignclient/FeignClientApplication.java
@@ -1,31 +1,16 @@
 package com.gmail.arthurstrokov.feignclient;
 
-import com.netflix.discovery.EurekaClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class FeignClientApplication {
 
-    @Autowired
-    private EurekaClient eurekaClient;
-
     public static void main(String[] args) {
         SpringApplication.run(FeignClientApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner commandLineRunner() {
-        return args -> {
-            System.out.println(eurekaClient.getApplication("cloud-configuration-server"));
-
-        };
     }
 }
